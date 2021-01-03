d27162010a6be2e6c31a60d2bb0c5a844f5294e2
EC2 Default User <ec2-user@ip-172-30-2-95.ap-northeast-1.compute.internal>
2021-01-03 13:40:59 +0000

    rails g scaffold news_items

---
+++ db/migrate/20210103133841_create_news_items.rb
@@ -0,0 +1,17 @@
+class CreateNewsItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :news_items do |t|
+      t.integer :news_site_id, null: false
+      t.string :title, null: false
+      t.text :description, null: false
+      t.text :link, null: false
+      t.datetime :published_at, null: false
+
+      t.timestamps
+      
+      t.index :news_site_id
+      t.index :published_at
+      t.index :link, unique: true
+    end
+  end
+end
