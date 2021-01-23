c83983af86431f05ce0cd90f42ae84e9110260b2
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-01-23 14:09:44 +0000

    サムネイル画像を表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,22 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-const VueCarousel  = window.VueCarousel.default;
-
-Vue.use(VueCarousel);
- 
-let app = new Vue({
-  el: '#app'
-});
-
-var messageSwitch = new Vue({
-  el: '#messageswitch',
-  data: {
-    toggle: true
-  },
-  methods: {
-    toggleBtn: function(){
-      this.toggle == true ? this.toggle = false : this.toggle = true
-    }
-  }
-})
